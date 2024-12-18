cd7612ee934a951018c25128f62800b6991f9502
buskingsue <abcdzip@naver.com>
2024-12-18 16:41:49 +0900

    주석작업

--- job01_crawling_headline.py
+++ job01_crawling_headline.py
@@ -3,17 +3,28 @@ import requests
 import re
 import pandas as pd
 import datetime
-
+'''
+BeautifulSoup: HTML 파싱.
+requests: HTTP 요청.
+re: 정규식을 통한 문자열 처리.
+pandas: 데이터프레임으로 데이터 관리.
+datetime: 파일명에 현재 날짜를 포함시키기 위해 사용.
+'''
 category = ['Politics', 'Economic', 'Social', 'Culture', 'World', 'IT']
+#카테고리와 데이터프레임 초기화
 url = 'https://news.naver.com/section/100'
-
+#카테고리 이름 정의.
+#수집된 모든 데이터를 저장할 빈 데이터프레임 생성.
 df_titles = pd.DataFrame ()
 
 for i in range(6): #0부터 5까지 6개
     url = 'https://news.naver.com/section/10{}'.format(i)
     resp = requests.get(url)
     soup = BeautifulSoup(resp.text,'html.parser')
+    # 해당 카테고리의 URL에 요청을 보내고, HTML 내용을 BeautifulSoup을 이용해 파싱.
     title_tags = soup.select('.sa_text_strong')
+    #CSS 선택자를 이용해 뉴스 제목을 추출하고, 텍스트 내용을 리스트로 정리.
+
     titles = []
     for title_tag in title_tags:
         title = title_tag.text
@@ -22,12 +33,19 @@ for i in range(6): #0부터 5까지 6개
         titles.append(title)
     df_section_titles = pd.DataFrame(titles, columns=['titles'])
     df_section_titles['category'] = category[i]
+    #각 카테고리별 뉴스 데이터를 데이터프레임으로 저장하고, 'category' 컬럼 추가.
     df_titles = pd.concat([df_titles, df_section_titles], axis = 'rows', ignore_index= True)
+    #카테고리별 데이터프레임을 하나의 데이터프레임으로 합침.
+
 print(df_titles.head())
 df_titles.info()
 print(df_titles['category'].value_counts())
+#수집된 데이터 확인.
+
 df_titles.to_csv('./crawling_data/naver_headline_news_{}.csv'.format(
     datetime.datetime.now().strftime('%Y%m%d')), index=False)
+#CSV 파일 저장
+
 #str format time
 '''        
 resp = requests.get(url)
