1264a38fc149643537d709a12815525282339d50
buskingsue <abcdzip@naver.com>
2024-12-20 12:26:19 +0900

    kkma 추가

--- job03_pre.py
+++ job03_pre.py
@@ -4,7 +4,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 import pickle
 from tensorflow.keras.utils import to_categorical
-from konlpy.tag import Okt
+from konlpy.tag import Okt, Kkma
 #open korea tokenizer
 import jpype
 
@@ -33,7 +33,11 @@ okt_x = : okt_x라는 변수에 값을 할당하고 있습니다.
 okt.morphs(X[0]) : okt 객체의 morphs 메서드는 주어진 텍스트 X[0]을 형태소 단위로 분해합니다. X[0]은 입력 데이터 리스트에서 첫 번째 요소입니다.
 즉, 이 코드는 X[0]에 있는 텍스트를 형태소 단위로 분석하여 그 결과를 okt_x에 저장하는 역할을 합니다.
 '''
-print(okt_x)
+print('okt_x :', okt_x)
+
+kkma = Kkma()
+kkma_x = kkma.morphs(X[0])
+print('kkma :', kkma_x)
 exit()
 
 encoder = LabelEncoder()
