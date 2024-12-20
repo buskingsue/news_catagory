3b4a1ab6770c810c81be9b6bad042783b9f76039
buskingsue <abcdzip@naver.com>
2024-12-20 16:33:27 +0900

    model_learning

--- job04_model_learning.py
+++ job04_model_learning.py
@@ -1,3 +1,5 @@
+from tabnanny import verbose
+
 import numpy as np
 from tensorflow.keras.models import *
 from tensorflow.keras.layers import *
@@ -35,6 +37,8 @@ model.summary()
                     #categorical_crossentropy
 model.compile(loss='categorical_crossentropy',optimizer='adam', metrics=['accuracy'])
 fit_hist = model.fit(X_train, Y_train, batch_size=128, epochs=10, validation_data=(X_test, Y_test))
+score = model.evaluate(X_test, Y_test, verbose = 0)
+print('Final Test set accuracy', score[1])
 model.save('./models/news_category_classfication_model_{}.h5'.format(
 fit_hist.history['val_accuracy'][-1]))
 plt.plot(fit_hist.history['val_accuracy'], label='val_accuracy')
